080192b604c99c177060ae683736a5e11029934a
natcha phonkamhaeng <natcha.phonkamhaeng@gmail.com>
2023-06-04 11:53:31 +0700

    update card body

--- src/app.py
+++ src/app.py
@@ -4,124 +4,81 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ],
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX],
 		meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 
 # server = app.server
 
 df = pd.read_csv('data/nobel.csv')
+dff = df.groupby(['nobel year', 'category', 'gender']).size().reset_index(name='count')
+fig = px.histogram(dff, x="nobel year", y='count', color="gender",
+                   marginal="rug", # or violin, rug
+                   hover_data=dff.columns, labels={'count': 'Count of Gender'}).update_layout(yaxis_title='Count of Gender', paper_bgcolor='#F8F8FF')
 
-#-------------- For Line Chart -----------------------------------------
-dfGroup = df.groupby(['nobel year', 'gender']).size().to_frame(name='count').reset_index()
-figLine = px.line(dfGroup, x='nobel year', y='count', color='gender', labels={'count': 'Numbers for Nobel Prize'})
-figLine.update_layout(
-			font=dict(color='white'), 
-			margin=dict(l=0, r=0, t=40, b=3),
-			paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(255,255,255,0.5)')
+figSun = px.sunburst(df, path=['category', 'gender']).update_layout(margin=dict(l=0, r=0, t=0, b=0),paper_bgcolor='#F8F8FF')
 
-#-------------- For Sunburst Chart -----------------------------------------
-figSun = px.sunburst(df, path=['category', 'gender'])
-figSun.update_layout(font=dict(color='white'), 
-			margin=dict(l=0, r=0, t=0, b=0),
-			paper_bgcolor='rgba(0,0,0,0)',) # making eges transparent
-            #plot_bgcolor='rgb(255,255,255)')
-figSun.update_traces(marker_line_color='rgba(255,0,255,0.3)') # set the color border
+fig2 = px.histogram(dff, x="nobel year", y='count', color="category",
+                   marginal="rug", # or violin, rug
+                   hover_data=dff.columns)
 
-#-------------- For Choropeth Map -----------------------------------------
-dfChoro = df.groupby(['alpha-3','birth_country']).size().reset_index(name='count')
-figChoro = px.choropleth(dfChoro, locations='alpha-3', color='count', hover_name='birth_country')
-#figChoro.update_geos(fitbounds="locations", visible=True)
-figChoro.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, geo=dict(bgcolor= 'rgba(0,0,0,0)'), paper_bgcolor='rgba(0,0,0,0)',)
-#figChoro.update_traces(marker_line_width=0)
-
-#--------------------------------------------------------------------------
-
-navbar = dbc.NavbarSimple(
-    children=[
-        #dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("All", href="#", id='all'),
-                dbc.DropdownMenuItem("Physics", href="#", id='physics'),
-                dbc.DropdownMenuItem("Chemistry", href="#", id='chemistry'),
-                dbc.DropdownMenuItem("Peace", href="#", id='peace'),
-                dbc.DropdownMenuItem("Literature", href="#", id='literature'),
-                dbc.DropdownMenuItem("Economics", href="#", id='economics'),
-                dbc.DropdownMenuItem("Medicine", href="#", id='medicine'),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="Category",
-        ),
-    ],
-    brand="The Nobel Prize",
-    brand_href="#",
-    color="primary",
-    dark=True,
-    class_name='mb-2',
-)
-
-container_style = {
-	'margin-left': '0rem',
-	#'margin-right': '0rem',
-}
-
-app.layout = html.Div([
-	navbar,
-	dbc.Container([
-		dbc.Row([
-			dbc.Col([
-				dcc.Graph(figure=figSun, id='my-sun')
-				], width=3),			
-			dbc.Col([
-				dcc.Graph(figure=figLine, id='my-graph2')
-				], width=9)
-			], class_name='mb-3'),
-		dbc.Row([
-			dbc.Col([
-				dcc.Graph(figure=figChoro, id='my-graph3')
-				])
+app.layout = dbc.Container([
+	dbc.Row([
+		dbc.Col([
+			html.H1(['The Nobel Prize'])
+			])
+		],
+		class_name='mt-3 mb-2'
+		),
+	dbc.Row([
+		dbc.Col([
+			dbc.Card([
+				dbc.CardBody([
+					dbc.Row([
+						dbc.Col([
+							html.H3(['Gender'])
+							]),
+						]),
+					html.Hr(),
+					dbc.Row([
+						dbc.Col([
+							dcc.Graph(figure=fig, id='gender-fig')
+							])
+						])
+					])
+				],
+				class_name='bg-card mb-5'
+				),
+			dbc.Card([
+				dbc.CardBody([
+					dbc.Row([
+						dbc.Col([
+							html.H3(['Gender and Category'])
+							])
+						]),
+					html.Hr(),
+					dbc.Row([
+						dbc.Col([
+							html.P(['Year']),
+							dcc.Dropdown(options=['1990', '1991', '1992']),
+							html.P(['Category'], className='mt-3'),
+							dcc.Dropdown(options=['Physic', 'Medical'])
+							], width=2),
+						dbc.Col([
+							dcc.Graph(figure=figSun, id='cate-sun')
+							], width=3),
+						dbc.Col([
+							dcc.Graph(figure=fig2, id='cate-fig')
+							], width=7)
+						])
+					])
+				],
+				class_name='bg-card mb-5'
+				)
 			])
 		])
 	])
 	
 
-@app.callback(
-    Output('my-sun', "figure"),
-    Output('my-graph2', 'figure'),
-    Output('my-graph3', 'figure'),
-    Input('all', 'n_clicks'),
-    Input('physics', 'n_clicks'),
-    Input('chemistry', 'n_clicks'),
-    Input('peace', 'n_clicks'),
-    Input('literature', 'n_clicks'),
-    Input('economics', 'n_clicks'),
-    Input('medicine', 'n_clicks'),
-    prevent_initial_update=True
-)
-def update_label(n_all, n_physics, n_chem, n_peace, n_lite, n_econ, n_med):
-	ctx = dash.callback_context
-
-	if not ctx.triggered:
-		
-		return figSun, figLine, figChoro
-	elif ctx.triggered:
-		button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-		#print(button_id)
-		if button_id == 'physics':
-			
-			# button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-			# print(button_id)
-			return figSun, figLine, figChoro
-		if button_id == 'chemistry':
-			data_canada = px.data.gapminder().query("country == 'Canada'")
-			figBar = px.bar(data_canada, x='year', y='pop')
-			
-			return figSun, figLine, figBar
-		else:
-			return figSun, figLine, figChoro
-	
 
 if __name__ == '__main__':
     app.run_server(debug=True)
