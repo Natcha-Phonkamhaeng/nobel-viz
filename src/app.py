513980e64bd0d86873a08fc2640727d4617f8c98
natcha phonkamhaeng <natcha.phonkamhaeng@gmail.com>
2023-06-24 19:16:47 +0700

    change layout to Tab

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 import dash_ag_grid as dag
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX],
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True, 
 		meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 
 server = app.server
@@ -36,21 +36,33 @@ columnDefs = [
 ]
 
 def figMap():
-	# world map of country category
+	# world map of Country Category
 	dfMap = df.groupby(['alpha-3','birth_country']).size().reset_index(name='count')
 	figMap = px.choropleth(dfMap, locations='alpha-3', color='count', hover_name='birth_country')
 	figMap.update_layout(paper_bgcolor='rgb(248,248,255)')
 	return figMap
 
-app.layout = dbc.Container([
-	dbc.Row([
-		dbc.Col([
-			html.H1(['The Nobel Prize'])
-			])
-		],
-		class_name='mt-3 mb-2'
-		),
-	dbc.Row([
+def figGender():
+	# histogram for Gender Category
+	dfGroup = df.groupby(['nobel year', 'category', 'gender']).size().reset_index(name='count')
+	figGender = px.histogram(dfGroup, x="nobel year", y='count', color="gender",
+                   marginal="rug", # or violin, rug
+                   hover_data=dfGroup.columns, labels={'count': 'Count of Gender'}).update_layout(yaxis_title='Count of Gender', paper_bgcolor='#F8F8FF')
+	return figGender
+
+def figCat():
+	# Sun burst chart
+	figSun = px.sunburst(df, path=['category', 'gender']).update_layout(margin=dict(l=0, r=0, t=0, b=0),paper_bgcolor='#F8F8FF')
+	figSun.update_traces(marker_colors=[color_mapping[cat] for cat in figSun.data[-1].labels])
+	# Bar chart
+	dfGroup = df.groupby(['nobel year', 'category']).size().reset_index(name='count')
+	figBar = px.histogram(dfGroup, x='nobel year', y='count', color='category', barmode='group',
+			labels={'count': 'Number of Nobel Prize Received'},
+			color_discrete_map=color_mapping)
+	figBar.update_layout(yaxis_title='Number of Nobel Prize Received',paper_bgcolor='#F8F8FF')
+	return figSun, figBar
+
+gender_layout = dbc.Row([
 		dbc.Col([
 			dbc.Card([
 				dbc.CardBody([
@@ -62,19 +74,22 @@ app.layout = dbc.Container([
 					html.Hr(),
 					dbc.Row([
 						dbc.Col([
-							dcc.Graph(id='gender-fig')
+							dcc.Graph(figure=figGender())
 							])
 						])
 					])
-				],
-				class_name='bg-card mb-5'
-				),
+				])
+			])
+		])
+
+category_layout = dbc.Row([
+		dbc.Col([
 			dbc.Card([
 				dbc.CardBody([
 					dbc.Row([
 						dbc.Col([
 							html.H3(['Category'])
-							])
+							]),
 						]),
 					html.Hr(),
 					dbc.Row([
@@ -86,16 +101,15 @@ app.layout = dbc.Container([
 							dcc.Graph(figure={}, id='cat-sun')
 							], width=3),
 						dbc.Col([
-							dcc.Graph(figure={}, id='cat-fig')
+							dcc.Graph(figure={}, id='cat-bar')
 							], width=7)
 						])
+						])
 					])
-				],
-				class_name='bg-card mb-5'
-				)
+				])
 			])
-		]),
-	dbc.Row([
+
+country_layout = dbc.Row([
 		dbc.Col([
 			dbc.Card([
 				dbc.CardBody([
@@ -116,7 +130,7 @@ app.layout = dbc.Container([
 					html.Br(),
 					dbc.Row([
 						dbc.Col([
-							dcc.Graph(figure=figMap(), id='country-map'),
+							dcc.Graph(figure=figMap()),
 							html.Br(),
 							dag.AgGrid(
 								id='grid-table', 
@@ -133,56 +147,54 @@ app.layout = dbc.Container([
 				)
 			])
 		])
+
+
+app.layout = dbc.Container([
+	dbc.Row([
+		dbc.Col([
+			html.H1(['The Nobel Prize'])
+			])
+		],
+		class_name='mt-3 mb-2'
+		),
+	dcc.Tabs(id='input-tabs', value='gender-tab', children=[
+		dcc.Tab(label='Gender', value='gender-tab'),
+		dcc.Tab(label='Category', value='category-tab'),
+		dcc.Tab(label='Country', value='country-tab')
+		]),
+	html.Div(id='output-tabs', children={})
 	])
 
-# callback for Category
+# callback for updating interactive Category
 @callback(
-	Output('gender-fig', 'figure'),
 	Output('cat-sun', 'figure'),
-	Output('cat-fig', 'figure'),
-	Input('dropdown_year', 'value')
+	Output('cat-bar', 'figure'),
+	Input('dropdown_year', 'value'),
 	)
-def update_graph(select_year):
+def update_cat(select_year):
 	dff = df.copy()
-
-	#------------------------- Graph on Gender #-------------------------
-	dfGroup = df.groupby(['nobel year', 'category', 'gender']).size().reset_index(name='count')
-	fig = px.histogram(dfGroup, x="nobel year", y='count', color="gender",
-                   marginal="rug", # or violin, rug
-                   hover_data=dfGroup.columns, labels={'count': 'Count of Gender'}).update_layout(yaxis_title='Count of Gender', paper_bgcolor='#F8F8FF')
-	
-	#------------------------- Graph on Category #-------------------------
-	# Sun burst chart
-	figSun = px.sunburst(dff, path=['category', 'gender']).update_layout(margin=dict(l=0, r=0, t=0, b=0),paper_bgcolor='#F8F8FF')
-	figSun.update_traces(marker_colors=[color_mapping[cat] for cat in figSun.data[-1].labels])
-	# bar chart
-	dffGroup = dff.groupby(['nobel year', 'category']).size().reset_index(name='count')
-	fig2 = px.histogram(dffGroup, x='nobel year', y='count', color='category', barmode='group',
-			labels={'count': 'Number of Nobel Prize Received'},
-			color_discrete_map=color_mapping)
-	fig2.update_layout(yaxis_title='Number of Nobel Prize Received',paper_bgcolor='#F8F8FF')
-	
 	if select_year:
 		# update sunburst chart
 		figSun = px.sunburst(dff[dff['nobel year'] == select_year], path=['category', 'gender']).update_layout(margin=dict(l=0, r=0, t=0, b=0),paper_bgcolor='#F8F8FF')
 		figSun.update_traces(marker_colors=[color_mapping[cat] for cat in figSun.data[-1].labels])
 		# update barchart
+		dffGroup = dff.groupby(['nobel year', 'category']).size().reset_index(name='count')
 		mark = (dffGroup['nobel year'] == select_year)
-		fig2 = px.histogram(dffGroup[mark], x='nobel year', y='count', color='category', barmode='group',
+		figBar = px.histogram(dffGroup[mark], x='nobel year', y='count', color='category', barmode='group',
 				labels={'count': 'Number of Nobel Prize Received'},
 				color_discrete_map=color_mapping)
-		fig2.update_layout(yaxis_title='Number of Nobel Prize Received',paper_bgcolor='#F8F8FF')
-		fig2.update_xaxes(visible=False)
-		return fig, figSun, fig2
+		figBar.update_layout(yaxis_title='Number of Nobel Prize Received',paper_bgcolor='#F8F8FF')
+		figBar.update_xaxes(visible=False)
+		return figSun, figBar
 	else:
-		return fig, figSun, fig2
+		return figCat()
 
-#callback for Country
+# callback for updating interactive Country
 @callback(
 	Output('grid-table', 'rowData'),
 	Input('dropdown-country', 'value')
 	)
-def update_map(select_country):
+def update_country(select_country):
 	dff = df.copy()
 
 	if select_country:
@@ -191,6 +203,20 @@ def update_map(select_country):
 		return dff.to_dict('records')
 	else:
 		return dff.to_dict('records')
+
+
+@callback(
+	Output('output-tabs', 'children'),
+	Input('input-tabs', 'value')
+	)
+def content(tab):
+	if tab == 'gender-tab':
+		return gender_layout
+	elif tab == 'category-tab':
+		return category_layout
+	elif tab == 'country-tab':
+		return country_layout
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
